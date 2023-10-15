bc2b42e900e125b72f174a86d8c7a27c03efe5c5
Arpita Jadhav <89975545+arpitajadhav155@users.noreply.github.com>
2023-10-15 11:30:46 +0530

    Add files via upload

---
+++ bubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bubbleSort {
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+}
