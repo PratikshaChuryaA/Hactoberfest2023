1b382f2f1ce755875982f72641fc0d7be611d5f9
Subash Paudel Chhetri <69474140+spyrochhetri@users.noreply.github.com>
2023-10-17 14:51:22 -0400

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,36 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2  # Find the middle of the array
+        left_half = arr[:mid]  # Divide the array into two halves
+        right_half = arr[mid:]
+
+        merge_sort(left_half)  # Recursive call on the left half
+        merge_sort(right_half)  # Recursive call on the right half
+
+        i = j = k = 0
+
+        # Merge the two halves back together
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+
+        # Check for any remaining elements
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+
+# Example usage:
+arr = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(arr)
+print("Sorted array is:", arr)
