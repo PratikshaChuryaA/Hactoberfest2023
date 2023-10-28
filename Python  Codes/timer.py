3e848a2945b468fce74792092686c9452b7d44d1
nilam576 <71421769+nilam576@users.noreply.github.com>
2023-10-29 00:19:43 +0530

    Create timer.py

---
+++ Python  Codes/timer.py
@@ -0,0 +1,13 @@
+import time
+
+def countdown(t):
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print("Timer completed!")
+
+countdown(120)  # Set the timer duration in seconds
